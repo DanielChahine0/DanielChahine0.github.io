34dad8df50c799937d0bb48d9d2e3815d65d4060
DanielChahine0 <121309471+DanielChahine0@users.noreply.github.com>
2025-07-16 23:16:23 -0400

    Created all the styling

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,48 @@
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from 'react';
+
+export const ThemeToggle = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    // Check the saved theme preference in localStorage when the component mounts
+    // and set the initial state accordingly
+    useEffect(() => {
+        // Check the saved theme preference in localStorage
+        const savedTheme = localStorage.getItem("theme");
+        if (savedTheme === "dark") {
+            document.documentElement.classList.add("dark");
+            setIsDarkMode(true);
+        } else {
+            document.documentElement.classList.remove("dark");
+            setIsDarkMode(false);
+        }
+    }, []);
+
+    const toggleTheme = () => {
+        if (isDarkMode) {
+            // Toggle to light mode
+            document.documentElement.classList.remove("dark");
+            // Save the theme preference in localStorage to persist it when reloading the page
+            localStorage.setItem("theme", "light");
+            setIsDarkMode(false);
+        }
+        else {
+            // Toggle to dark mode
+            document.documentElement.classList.add("dark");
+            // Save the theme preference in localStorage to persist it when reloading the page
+            localStorage.setItem("theme", "dark");
+            setIsDarkMode(true);
+        }
+    };
+
+    return (
+        <button onClick={toggleTheme}> 
+
+            {isDarkMode ? (
+                <Sun className="h-6 w-6 text-yellow-300"/> 
+            ) : ( 
+                <Moon className="h-6 w-6 text-blue-900" />
+            )}
+        </button>
+    );
+}
---
+++ src/lib/utils.js
@@ -0,0 +1,3 @@
+export const cn = () => {
+    return "This is a utility function to combine class names.";
+}
