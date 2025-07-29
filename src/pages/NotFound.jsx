d4ca35b825ed21a7ef26483173da0f23b732d6cb
DanielChahine0 <121309471+DanielChahine0@users.noreply.github.com>
2025-07-29 12:42:05 -0400

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,19 @@
+
 import { PageTransition } from "../components/PageTransition";
+import { NavBar } from "../components/NavBar";
+import { Footer } from "../components/Footer";
 
 export const NotFound = () => {
   return (
-    <PageTransition>
-      <div>NotFound</div>
-    </PageTransition>
+    <>
+      <NavBar />
+      <PageTransition>
+        <div style={{ minHeight: "60vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+          <h1 style={{ fontSize: "3rem", marginBottom: "1rem" }}>404</h1>
+          <p style={{ fontSize: "1.5rem", color: "#888" }}>Sorry, the page you are looking for was not found.</p>
+        </div>
+      </PageTransition>
+      <Footer />
+    </>
   );
 }
