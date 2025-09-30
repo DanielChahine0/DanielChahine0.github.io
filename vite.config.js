974d9ceb582630266d246a220556a40498d5bf7b
DanielChahine0 <121309471+DanielChahine0@users.noreply.github.com>
2025-09-29 23:27:08 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
+// When using ESM, __dirname is not defined. Use import.meta.url to resolve paths.
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
